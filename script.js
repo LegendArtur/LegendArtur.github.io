2532aafc294efd5e557a2c4b21cb536bf4a33713
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2022-09-28 09:43:58 -0400

    fixed screen flashing
    -problem was: background animation

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+function toggleNavCurtain() {
+    document.getElementById('nav-curtain').classList.toggle('active')
+}
 let menutoggle = document.querySelector('.toggle');
+let menubutton = document.querySelector('.menu-content');
+
+menubutton.onclick = function () {
+    document.getElementById('nav-curtain').classList.toggle('active')
+}
 menutoggle.onclick = function () {
     document.getElementById('nav-curtain').classList.toggle('active')
 }
 
-let section_number = 0;
+// let section_number = 0;
 // let old_section_number = 0;
 
 // window.addEventListener("scroll", function() {
