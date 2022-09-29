475a8042b124ffae5b28d3b78c02fff3af861d2d
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2022-09-28 20:47:51 -0400

    implementing anime.js lib
    -better organization of files
    -including the anime.js library for future animation

---
+++ scripts/animations.js
@@ -0,0 +1,6 @@
+//import anime from 'animejs/lib/anime.es.js';
+
+//const anime = require('animejs');
+
+anime({
+  });
---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+console.log(window.anime !== undefined)
+
+function toggleNavCurtain() {
+    document.getElementById('nav-curtain').classList.toggle('active')
+}
+let menutoggle = document.querySelector('.toggle');
+let menubutton = document.querySelector('.menu-content');
+
+menubutton.onclick = function () {
+    document.getElementById('nav-curtain').classList.toggle('active')
+}
+menutoggle.onclick = function () {
+    document.getElementById('nav-curtain').classList.toggle('active')
+}
+
+
+  
