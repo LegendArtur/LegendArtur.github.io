e67e285a50878c77c884c24fdb9d691a6e4b84cf
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2022-10-05 21:46:53 -0400

    adding waypoints library

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,21 +1,43 @@
+// //scroll percent
+// const getPageScrollPercent = function (div) {
+//     return window.pageYOffset - div.offsetTop;
+// }
 
-const sections = document.querySelectorAll(".section");
+// //scroll trigger amount
+// const getPageScrollLength = function (scrollPercent, speed = 100, offset = 0) {
+//     return (scrollPercent + offset) / speed;
+// }
+
+// //scrolling animation
+// const sections = document.querySelectorAll(".section");
+
+
+
+// window.onscroll = function () {
+//     sections.forEach((section) => {
+//         let scrollPercent = getPageScrollPercent(section);
+//         console.log(section.id + " : " + scrollPercent);
+//         if (scrollPercent > 0) {
+//             section.classList.add('seen');
+//         }
+//         if (scrollPercent < 0) {
+//             section.classList.remove('seen');
+//         }
+//         text_animation.seek(getPageScrollLength(scrollPercent, 100, 0) * text_animation.duration);
+//     });
+// }
+
 
 var text_animation = anime({
-    targets: sections,
-    translateX: 270,
+    targets: "#section2",
+    translateX: 250,
     duration: 3000,
-    autoplay: false
+    autoplay: false,
 });
-
-
-const animateOnScroll = function(div, speed=100, offset=0) {
-    const scrollPercent = window.pageYOffset - div.offsetTop;
-    return (scrollPercent + offset) / speed;
-}
-
-window.onscroll = function () {
-    sections.forEach((section) => {
-        text_animation.seek(animateOnScroll(section, 1000, 100) * text_animation.duration);
-    });
-}
+var waypoint = new Waypoint({
+    element: document.getElementById('section2'),
+    handler: function () {
+        text_animation.play();
+        console.log('Basic waypoint triggered')
+    }
+})
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-console.log(window.anime !== undefined)
-
 function toggleNavCurtain() {
     document.getElementById('nav-curtain').classList.toggle('active')
 }
@@ -9,23 +7,8 @@ let menubutton = document.querySelector('.menu-content');
 menubutton.onclick = function () {
     document.getElementById('nav-curtain').classList.toggle('active')
 }
+
 menutoggle.onclick = function () {
     document.getElementById('nav-curtain').classList.toggle('active')
 }
 
-// var item = document.getElementById("");
-// item.addEventListener("mouseover", func, false);
-// item.addEventListener("mouseout", func1, false);
-
-// function func()
-// {  // not needed since item is already global, 
-//    // I am assuming this is here just because it's sample code?
-//    // var item = document.getElementById("button"); 
-//    item.setAttribute("style", "background-color:blue;")
-// }
-
-// function func1()
-// {  
-//    item.setAttribute("style", "background-color:green;")
-// }
-  
