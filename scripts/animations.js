af5644eb52207311cdc280164390f78911e69dcd
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2022-10-18 01:15:41 -0400

    Even better responsiveness for image

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,13 +1,12 @@
 
-var lineDrawing = anime({
-    targets: 'path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 5000,
-    begin: function(anim) {
-      document.querySelector('path').setAttribute("stroke", "blue");
-    },
-    autoplay: true
-  });
 
-window.onload = console.log('yes');
+var lineDrawing = anime({
+  targets: 'path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 5000,
+  begin: function(anim) {
+    document.querySelector('path').setAttribute("stroke", "white");
+  },
+  autoplay: true
+});
